629dd9a8311e2d0e3e0afe77567359c0a6e07934
Santiago <94338894+CapitanSopa@users.noreply.github.com>
2025-03-20 03:21:20 -0600

    Add files via upload

---
+++ P05-422016430.cpp
@@ -0,0 +1,266 @@
+﻿/*
+Práctica 5: Optimización y Carga de Modelos
+*/
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <vector>
+#include <math.h>
+
+#include <glew.h>
+#include <glfw3.h>
+
+#include <glm.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+//para probar el importer
+//#include<assimp/Importer.hpp>
+
+#include "Window.h"
+#include "Mesh.h"
+#include "Shader_m.h"
+#include "Camera.h"
+#include "Sphere.h"
+#include"Model.h"
+#include "Skybox.h"
+
+const float toRadians = 3.14159265f / 180.0f;
+//float angulocola = 0.0f;
+Window mainWindow;
+std::vector<Mesh*> meshList;
+std::vector<Shader> shaderList;
+
+Camera camera;
+Model Goddard_M;
+Model carro, cabeza, cofre, llanta1, llanta2, llanta3, llanta4, car;
+
+Skybox skybox;
+
+//Sphere cabeza = Sphere(0.5, 20, 20);
+GLfloat deltaTime = 0.0f;
+GLfloat lastTime = 0.0f;
+static double limitFPS = 1.0 / 60.0;
+
+//Declaraciones
+
+
+// Vertex Shader
+static const char* vShader = "shaders/shader_m.vert";
+
+// Fragment Shader
+static const char* fShader = "shaders/shader_m.frag";
+
+
+
+
+
+void CreateObjects()
+{
+	unsigned int indices[] = {
+		0, 3, 1,
+		1, 3, 2,
+		2, 3, 0,
+		0, 1, 2
+	};
+
+	GLfloat vertices[] = {
+		//	x      y      z			u	  v			nx	  ny    nz
+			-1.0f, -1.0f, -0.6f,	0.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, -1.0f, 1.0f,		0.5f, 0.0f,		0.0f, 0.0f, 0.0f,
+			1.0f, -1.0f, -0.6f,		1.0f, 0.0f,		0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f,		0.5f, 1.0f,		0.0f, 0.0f, 0.0f
+	};
+
+	unsigned int floorIndices[] = {
+		0, 2, 1,
+		1, 2, 3
+	};
+
+	GLfloat floorVertices[] = {
+		-10.0f, 0.0f, -10.0f,	0.0f, 0.0f,		0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, -10.0f,	10.0f, 0.0f,	0.0f, -1.0f, 0.0f,
+		-10.0f, 0.0f, 10.0f,	0.0f, 10.0f,	0.0f, -1.0f, 0.0f,
+		10.0f, 0.0f, 10.0f,		10.0f, 10.0f,	0.0f, -1.0f, 0.0f
+	};
+
+
+	Mesh* obj1 = new Mesh();
+	obj1->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj1);
+
+	Mesh* obj2 = new Mesh();
+	obj2->CreateMesh(vertices, indices, 32, 12);
+	meshList.push_back(obj2);
+
+	Mesh* obj3 = new Mesh();
+	obj3->CreateMesh(floorVertices, floorIndices, 32, 6);
+	meshList.push_back(obj3);
+
+
+}
+
+
+void CreateShaders()
+{
+	Shader* shader1 = new Shader();
+	shader1->CreateFromFiles(vShader, fShader);
+	shaderList.push_back(*shader1);
+}
+
+
+
+int main()
+{
+	mainWindow = Window(1366, 768); // 1280, 1024 or 1024, 768
+	mainWindow.Initialise();
+
+	CreateObjects();
+	CreateShaders();
+
+	camera = Camera(glm::vec3(0.0f, 0.5f, 7.0f), glm::vec3(0.0f, 1.0f, 0.0f), -60.0f, 0.0f, 0.3f, 1.0f);
+
+	/*Goddard_M = Model();
+	Goddard_M.LoadModel("Models/goddard_base.obj");*/
+
+	carro = Model();
+	carro.LoadModel("Models/Carro.obj");
+
+	cofre = Model();
+	cofre.LoadModel("Models/Cofre.obj");
+
+	llanta1 = Model();
+	llanta1.LoadModel("Models/Llanta1.obj");
+
+	llanta2 = Model();
+	llanta2.LoadModel("Models/Llanta2.obj");
+
+	llanta3 = Model();
+	llanta3.LoadModel("Models/Llanta3.obj");
+
+	llanta4 = Model();
+	llanta4.LoadModel("Models/Llanta4.obj");
+	
+	car = Model();
+	car.LoadModel("Models/Car.obj");
+
+
+	std::vector<std::string> skyboxFaces;
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_rt.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_lf.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_dn.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_up.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_bk.tga");
+	skyboxFaces.push_back("Textures/Skybox/cupertin-lake_ft.tga");
+
+	skybox = Skybox(skyboxFaces);
+
+	GLuint uniformProjection = 0, uniformModel = 0, uniformView = 0, uniformEyePosition = 0,
+		uniformSpecularIntensity = 0, uniformShininess = 0;
+	GLuint uniformColor = 0;
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(), 0.1f, 1000.0f);
+
+
+	glm::mat4 model(1.0);
+	glm::mat4 modelaux(1.0);
+	glm::vec3 color = glm::vec3(1.0f, 1.0f, 1.0f);
+
+	////Loop mientras no se cierra la ventana
+	while (!mainWindow.getShouldClose())
+	{
+		GLfloat now = glfwGetTime();
+		deltaTime = now - lastTime;
+		deltaTime += (now - lastTime) / limitFPS;
+		lastTime = now;
+
+		//Recibir eventos del usuario
+		glfwPollEvents();
+		camera.keyControl(mainWindow.getsKeys(), deltaTime);
+		camera.mouseControl(mainWindow.getXChange(), mainWindow.getYChange());
+
+		// Clear the window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		// Dibuja Skybox
+		skybox.DrawSkybox(camera.calculateViewMatrix(), projection);
+		shaderList[0].UseShader();
+		uniformModel = shaderList[0].GetModelLocation();
+		uniformProjection = shaderList[0].GetProjectionLocation();
+		uniformView = shaderList[0].GetViewLocation();
+		uniformColor = shaderList[0].getColorLocation();
+
+		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
+		glUniformMatrix4fv(uniformView, 1, GL_FALSE, glm::value_ptr(camera.calculateViewMatrix()));
+
+		color = glm::vec3(0.5f, 0.5f, 0.5f);
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(0.0f, -2.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(30.0f, 1.0f, 30.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		meshList[2]->RenderMesh();
+
+		
+
+		//Carro
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(-20.0f, -2.0f, 0.0f));//altura del piso
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, mainWindow.getWheelsRotation()));
+		modelaux = model;
+		color = glm::vec3(1.0f, 0.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		carro.RenderModel();
+
+		// cofre
+		modelaux = model;
+		model = glm::translate(model, glm::vec3(0.0f, 7.9f, 5.9f));
+		model = glm::rotate(model, glm::radians(mainWindow.getCofreRotation()), glm::vec3(1.0f, 0.0f, 0.0f));
+		color = glm::vec3(0.5f, 0.0f, 0.0f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		cofre.RenderModel();
+
+		//Llanta1
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(6.6f, 3.0f, 10.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getWheel1Rotation()), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getWheelsRotation()), glm::vec3(5.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		color = glm::vec3(0.1f, 0.1f, 0.1f);
+		glUniform3fv(uniformColor, 1, glm::value_ptr(color));
+		llanta1.RenderModel();
+
+		//Llanta2
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-6.4f, 3.0f, 10.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getWheelsRotation()), glm::vec3(5.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getWheel2Rotation()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		llanta2.RenderModel();
+
+		//Llanta3
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(6.6f, 3.0f, -10.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getWheelsRotation()), glm::vec3(5.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getWheel3Rotation()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		llanta3.RenderModel();
+
+		//Llanta4
+		model = modelaux;
+		model = glm::translate(model, glm::vec3(-6.3f, 3.0f, -10.0f));		
+		model = glm::rotate(model, glm::radians(mainWindow.getWheelsRotation()), glm::vec3(5.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, glm::radians(mainWindow.getWheel4Rotation()), glm::vec3(1.0f, 0.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		llanta4.RenderModel();
+
+		glUseProgram(0);
+
+		mainWindow.swapBuffers();
+	}
+
+	return 0;
+}
---
+++ WindowE05-422016430.cpp
@@ -0,0 +1,331 @@
+#include "Window.h"
+
+Window::Window()
+{
+	width = 800;
+	height = 600;
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
+	rotax = 0.0f;
+	rotay = 0.0f;
+	rotaz = 0.0f;
+	articulacion1 = 0.0f;
+	articulacion2 = 0.0f;
+	articulacion3 = 0.0f;
+	articulacion4 = 0.0f;
+	articulacion5 = 0.0f;
+	articulacion6 = 0.0f;
+	wheel1Rotation = 0.0f;
+	wheel2Rotation = 0.0f;
+	wheel3Rotation = 0.0f;
+	wheel4Rotation = 0.0f;
+
+
+	
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+int Window::Initialise()
+{
+	//Inicialización de GLFW
+	if (!glfwInit())
+	{
+		printf("Falló inicializar GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Asignando variables de GLFW y propiedades de ventana
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//para solo usar el core profile de OpenGL y no tener retrocompatibilidad
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	//CREAR VENTANA
+	mainWindow = glfwCreateWindow(width, height, "Practica XX: Nombre de la práctica", NULL, NULL);
+
+	if (!mainWindow)
+	{
+		printf("Fallo en crearse la ventana con GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Obtener tamaño de Buffer
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	//asignar el contexto
+	glfwMakeContextCurrent(mainWindow);
+
+	//MANEJAR TECLADO y MOUSE
+	createCallbacks();
+
+
+	//permitir nuevas extensiones
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Falló inicialización de GLEW");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST); //HABILITAR BUFFER DE PROFUNDIDAD
+							 // Asignar valores de la ventana y coordenadas
+							 
+							 //Asignar Viewport
+	glViewport(0, 0, bufferWidth, bufferHeight);
+	//Callback para detectar que se está usando la ventana
+	glfwSetWindowUserPointer(mainWindow, this);
+}
+
+void Window::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, ManejaTeclado);
+	glfwSetCursorPosCallback(mainWindow, ManejaMouse);
+}
+
+GLfloat Window::getXChange()
+{
+	GLfloat theChange = xChange;
+	xChange = 0.0f;
+	return theChange;
+}
+
+GLfloat Window::getYChange()
+{
+	GLfloat theChange = yChange;
+	yChange = 0.0f;
+	return theChange;
+}
+
+void Window::ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	// Llantas
+	if (key == GLFW_KEY_1)
+	{
+		theWindow->wheel1Rotation += 10.0;
+	}
+	if (key == GLFW_KEY_2)
+	{
+		theWindow->wheel2Rotation += 10.0;
+	}
+	if (key == GLFW_KEY_3)
+	{
+		theWindow->wheel3Rotation += 10.0;
+	}
+	if (key == GLFW_KEY_4)
+	{
+		theWindow->wheel4Rotation += 10.0;
+	}
+	
+	if (key == GLFW_KEY_E)
+	{
+		theWindow->rotax += 10.0;
+	}
+	if (key == GLFW_KEY_R)
+	{
+		theWindow->rotay += 10.0; //rotar sobre el eje y 10 grados
+	}
+	if (key == GLFW_KEY_T)
+	{
+		theWindow->rotaz += 10.0;
+	}
+	if (key == GLFW_KEY_F)
+	{
+		theWindow->articulacion1 += 10.0;
+	}
+
+	if (key == GLFW_KEY_G)
+	{
+		theWindow->articulacion2 += 10.0;
+	}
+	if (key == GLFW_KEY_H)
+	{
+		theWindow->articulacion3 += 10.0;
+	}
+	if (key == GLFW_KEY_J)
+	{
+		theWindow->articulacion4 += 10.0;
+	}
+	if (key == GLFW_KEY_K)
+	{
+		theWindow->articulacion5 += 10.0;
+	}
+	if (key == GLFW_KEY_L)
+	{
+		theWindow->articulacion6 += 10.0;
+	}
+	//Pierna 1
+	if (key == GLFW_KEY_P) {
+		if (theWindow->leg1UpperRotation < 45.0f)
+			theWindow->leg1UpperRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_0) {
+		if (theWindow->leg1UpperRotation > -45.0f)
+			theWindow->leg1UpperRotation -= 5.0f;
+	}
+	if (key == GLFW_KEY_O) {
+		theWindow->leg1LowerRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_9) {
+		theWindow->leg1LowerRotation -= 5.0f;
+	}
+
+	// Pierna2
+	if (key == GLFW_KEY_E) {
+		if (theWindow->leg2UpperRotation < 45.0f)
+			theWindow->leg2UpperRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_Q) {
+		if (theWindow->leg2UpperRotation > -45.0f)
+			theWindow->leg2UpperRotation -= 5.0f;
+	}
+	if (key == GLFW_KEY_R) {
+		theWindow->leg2LowerRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_F) {
+		theWindow->leg2LowerRotation -= 5.0f;
+	}
+
+	// Pierna 3
+	if (key == GLFW_KEY_T) {
+		if (theWindow->leg3UpperRotation < 45.0f)
+			theWindow->leg3UpperRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_G) {
+		if (theWindow->leg3UpperRotation > -45.0f)
+			theWindow->leg3UpperRotation -= 5.0f;
+	}
+	if (key == GLFW_KEY_Y) {
+		theWindow->leg3LowerRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_H) {
+		theWindow->leg3LowerRotation -= 5.0f;
+	}
+
+	// Pierna 4
+	if (key == GLFW_KEY_U) {
+		if (theWindow->leg4UpperRotation < 45.0f)
+			theWindow->leg4UpperRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_J) {
+		if (theWindow->leg4UpperRotation > -45.0f)
+			theWindow->leg4UpperRotation -= 5.0f;
+	}
+	if (key == GLFW_KEY_I) {
+		theWindow->leg4LowerRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_K) {
+		theWindow->leg4LowerRotation -= 5.0f;
+	}
+
+	// OREJAS Y COLA
+	if (key == GLFW_KEY_Z) {
+		theWindow->leftEarRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_X) {
+		theWindow->leftEarRotation -= 5.0f;
+	}
+	if (key == GLFW_KEY_C) {
+		theWindow->rightEarRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_V) {
+		theWindow->rightEarRotation -= 5.0f;
+	}
+	if (key == GLFW_KEY_B) {
+		theWindow->tailRotation += 5.0f;
+	}
+	if (key == GLFW_KEY_N) {
+		theWindow->tailRotation -= 5.0f;
+	}
+	//Cabeza
+	if (key == GLFW_KEY_1) {
+		theWindow->headYaw += 5.0f;
+	}
+	if (key == GLFW_KEY_2) {
+		theWindow->headYaw -= 5.0f;
+	}
+	if (key == GLFW_KEY_3 && action == GLFW_PRESS) {
+		//if (theWindow->headPitch < 45.0f)
+		printf("Hola %d'\n", theWindow->headPitch);
+		theWindow->headPitch += 0.1f;
+			
+	}
+	if (key == GLFW_KEY_4 && action == GLFW_PRESS) {
+		//if (theWindow->headPitch > -45.0f)
+			theWindow->headPitch -= 0.1f;
+			printf(" %d'\n", headPitch);
+	}
+
+	if (key == GLFW_KEY_5) {
+		theWindow->headRoll += 5.0f;
+	}
+	if (key == GLFW_KEY_6) {
+		theWindow->headRoll -= 5.0f;
+	}
+
+
+	if (key == GLFW_KEY_D && action == GLFW_PRESS)
+	{
+		const char* key_name = glfwGetKeyName(GLFW_KEY_D, 0);
+		//printf("se presiono la tecla: %s\n",key_name);
+	}
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+			//printf("se presiono la tecla %d'\n", key);
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+			//printf("se solto la tecla %d'\n", key);
+		}
+	}
+}
+
+void Window::ManejaMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	theWindow->xChange = xPos - theWindow->lastX;
+	theWindow->yChange = theWindow->lastY - yPos;
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
+}
+
+
+Window::~Window()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+
+}
